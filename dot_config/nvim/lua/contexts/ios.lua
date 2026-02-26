2c35a57a61e747e4782e4906f4fc1194ff0df50d
Diogo Autilio <diautilio@gmail.com>
2026-02-26 10:10:00 -0300

    feat: add linting and formatting support for Swift in iOS configuration

--- dot_config/nvim/lua/contexts/ios.lua
+++ dot_config/nvim/lua/contexts/ios.lua
@@ -2,6 +2,8 @@ require("lazy").setup({
   { import = "plugins.ios.lsp" },
   { import = "plugins.ios.xcode" },
   { import = "plugins.ios.dap" },
+  { import = "plugins.ios.lint" },
+  { import = "plugins.ios.format" },
   { import = "plugins.shared.catppuccin" },
   { import = "plugins.shared.devicons" },
   { import = "plugins.shared.lualine" },
--- dot_config/nvim/lua/core/options.lua
+++ dot_config/nvim/lua/core/options.lua
@@ -1,9 +1,10 @@
 vim.opt.number = true
-vim.opt.relativenumber = true
+vim.opt.relativenumber = false
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.termguicolors = true
+vim.opt.mouse = "a"
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
