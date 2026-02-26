8e07cace0da4242c8a202824869e85ae002c8862
Diogo Autilio <diautilio@gmail.com>
2026-02-26 11:19:54 -0300

    feat: enhance LSP configuration with key mappings and additional dependencies

--- dot_config/nvim/lua/plugins/ios/lsp.lua
+++ dot_config/nvim/lua/plugins/ios/lsp.lua
@@ -1,25 +1,45 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-    },
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local root_files = {
-        "buildServer.json",
-        "Package.swift",
-        ".git",
-      }
-      local function get_root(fname)
-        return vim.fs.root(fname, root_files)
-      end
-
-      vim.lsp.config("sourcekit", {
-        cmd = { vim.trim(vim.fn.system("xcrun -f sourcekit-lsp")) },
-        root_dir = get_root,
-        capabilities = capabilities,
-      })
-    end,
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    { "antosha417/nvim-lsp-file-operations", config = true },
   },
+  config = function()
+    local lspconfig = vim.lsp.config
+    local cmp_nvim_lsp = require("cmp_nvim_lsp")
+    local capabilities = cmp_nvim_lsp.default_capabilities()
+    local opts = { noremap = true, silent = true }
+    local on_attach = function(_, bufnr)
+      opts.buffer = bufnr
+
+      opts.desc = "Show line diagnostics"
+      vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+
+      opts.desc = "Show documentation for what is under cursor"
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+
+      opts.desc = "Show LSP definition"
+      vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions trim_text=true<cr>", opts)
+    end
+
+    lspconfig("sourcekit", {
+      capabilities = capabilities,
+      on_attach = on_attach,
+      root_dir = function(_, callback)
+        callback(
+          require("lspconfig.util").root_pattern("buildServer.json", "Package.swift", "*.xcodeproj", "*.xcworkspace")(vim.fn.getcwd())
+            or require("lspconfig.util").find_git_ancestor(vim.fn.getcwd())
+        )
+      end,
+      cmd = { vim.trim(vim.fn.system("xcrun -f sourcekit-lsp")) },
+    })
+    vim.lsp.enable("sourcekit")
+
+    -- Diagnostic icons
+    local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+    for type, icon in pairs(signs) do
+      local hl = "DiagnosticSign" .. type
+      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+    end
+  end,
 }
