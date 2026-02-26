8a9fce11754de7507b8b59ab2801e9c07911f541
Diogo Autilio <diautilio@gmail.com>
2026-02-26 09:00:04 -0300

    feat: enhance key mappings and add fidget.nvim configuration

--- dot_config/nvim/lua/keymaps/ios.lua
+++ dot_config/nvim/lua/keymaps/ios.lua
@@ -5,22 +5,34 @@ vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover docs" })
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
 
-vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float)
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show diagnostics in a floating window" })
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
 
 -- Format Swift
-vim.keymap.set("n", "<leader>=", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>=", vim.lsp.buf.format, { desc = "Format Document" })
 
 -- Xcodebuild
-vim.keymap.set("n", "<leader>x", ":XcodebuildPicker<CR>")
-vim.keymap.set("n", "<leader>xb", ":XcodebuildBuild<CR>")
-vim.keymap.set("n", "<leader>xr", ":XcodebuildBuildRun<CR>")
-vim.keymap.set("n", "<leader>xt", ":XcodebuildTest<CR>")
-vim.keymap.set("n", "<leader>xl", ":XcodebuildToggleLogs<CR>")
-vim.keymap.set("n", "<leader>xc", ":XcodebuildCancel<CR>")
+vim.keymap.set("n", "<leader>X", "<cmd>XcodebuildPicker<cr>", { desc = "Show Xcodebuild Actions" })
+vim.keymap.set("n", "<leader>xf", "<cmd>XcodebuildProjectManager<cr>", { desc = "Show Project Manager Actions" })
 
-vim.keymap.set("n", "<leader>xs", ":XcodebuildSelectScheme<CR>")
-vim.keymap.set("n", "<leader>xd", ":XcodebuildSelectDevice<CR>")
-vim.keymap.set("n", "<leader>xp", ":XcodebuildSelectTestPlan<CR>")
-vim.keymap.set("n", "<leader>xX", ":XcodebuildSetup<CR>")
+vim.keymap.set("n", "<leader>xb", "<cmd>XcodebuildBuild<cr>", { desc = "Build Project" })
+vim.keymap.set("n", "<leader>xB", "<cmd>XcodebuildBuildForTesting<cr>", { desc = "Build For Testing" })
+vim.keymap.set("n", "<leader>xr", "<cmd>XcodebuildBuildRun<cr>", { desc = "Build & Run Project" })
+
+vim.keymap.set("n", "<leader>xt", "<cmd>XcodebuildTest<cr>", { desc = "Run Tests" })
+vim.keymap.set("v", "<leader>xt", "<cmd>XcodebuildTestSelected<cr>", { desc = "Run Selected Tests" })
+vim.keymap.set("n", "<leader>xT", "<cmd>XcodebuildTestClass<cr>", { desc = "Run This Test Class" })
+
+vim.keymap.set("n", "<leader>xl", "<cmd>XcodebuildToggleLogs<cr>", { desc = "Toggle Xcodebuild Logs" })
+vim.keymap.set("n", "<leader>xc", "<cmd>XcodebuildToggleCodeCoverage<cr>", { desc = "Toggle Code Coverage" })
+vim.keymap.set("n", "<leader>xC", "<cmd>XcodebuildShowCodeCoverageReport<cr>", { desc = "Show Code Coverage Report" })
+vim.keymap.set("n", "<leader>xe", "<cmd>XcodebuildTestExplorerToggle<cr>", { desc = "Toggle Test Explorer" })
+vim.keymap.set("n", "<leader>xs", "<cmd>XcodebuildFailingSnapshots<cr>", { desc = "Show Failing Snapshots" })
+
+vim.keymap.set("n", "<leader>xd", "<cmd>XcodebuildSelectDevice<cr>", { desc = "Select Device" })
+vim.keymap.set("n", "<leader>xp", "<cmd>XcodebuildSelectTestPlan<cr>", { desc = "Select Test Plan" })
+vim.keymap.set("n", "<leader>xq", "<cmd>Telescope quickfix<cr>", { desc = "Show QuickFix List" })
+
+vim.keymap.set("n", "<leader>xx", "<cmd>XcodebuildQuickfixLine<cr>", { desc = "Quickfix Line" })
+vim.keymap.set("n", "<leader>xa", "<cmd>XcodebuildCodeActions<cr>", { desc = "Show Code Actions" })
---
+++ dot_config/nvim/lua/plugins/shared/catppuccin.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha", -- latte, frappe, macchiato, mocha
+        transparent_background = false,
+        integrations = {
+          cmp = true,
+          treesitter = true,
+          telescope = { enabled = true },
+          nvimtree = true,
+          dap = true,
+          dap_ui = true,
+        },
+      })
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+}
