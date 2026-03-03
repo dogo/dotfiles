e35bc533dea9eec7dc1510b48c36ee373bab938d
Diogo Autilio <diautilio@gmail.com>
2026-03-03 20:48:23 -0300

    feat: update key mappings for psp Makefile and CMake

--- dot_config/nvim/lua/keymaps/psp.lua
+++ dot_config/nvim/lua/keymaps/psp.lua
@@ -1,2 +1,10 @@
-vim.keymap.set("n", "<leader>mb", ":!make<CR>")
-vim.keymap.set("n", "<leader>mr", ":!make run<CR>")
+-- Makefile
+vim.keymap.set("n", "<leader>mb", ":!make<CR>",       { desc = "Make build" })
+vim.keymap.set("n", "<leader>mr", ":!make run<CR>",   { desc = "Make run" })
+vim.keymap.set("n", "<leader>mc", ":!make clean<CR>", { desc = "Make clean" })
+
+-- CMake (preset psp)
+vim.keymap.set("n", "<leader>cb", ":!cmake --build --preset psp -j<CR>",                              { desc = "CMake build" })
+vim.keymap.set("n", "<leader>cc", ":!cmake --build --preset psp --target clean && echo 'Clean done!'<CR>",  { desc = "CMake clean" })
+vim.keymap.set("n", "<leader>ca", ":!cmake --preset psp && cmake --build --preset psp -j<CR>",        { desc = "CMake configure + build" })
+vim.keymap.set("n", "<leader>ci", ":!cmake --install build<CR>",                                      { desc = "CMake install" })
--- dot_config/nvim/lua/plugins/psp/c_cpp_lsp.lua
+++ dot_config/nvim/lua/plugins/psp/c_cpp_lsp.lua
@@ -5,12 +5,8 @@ return {
       vim.lsp.config("clangd", {
         cmd = { "clangd", "--target=psp" },
         filetypes = { "c", "cpp" },
-        root_dir = function(fname)
-          return vim.fs.root(fname, { ".git", "Makefile", "CMakeLists.txt" })
-        end,
-        settings = {},
+        root_markers = { ".git", "Makefile", "CMakeLists.txt" },
       })
-
       vim.lsp.enable("clangd")
     end,
   },
