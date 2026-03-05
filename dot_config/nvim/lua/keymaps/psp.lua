fa81df670b7e3d3e6b729ec61d021c6050f8b56a
Diogo Autilio <diautilio@gmail.com>
2026-03-05 09:27:08 -0300

    feat: refactor PSP key mappings to use a run function for Makefile and CMake commands

--- dot_config/nvim/lua/keymaps/psp.lua
+++ dot_config/nvim/lua/keymaps/psp.lua
@@ -1,10 +1,15 @@
+local function run(cmd)
+  vim.cmd("botright 15split | terminal " .. cmd)
+  vim.cmd("startinsert")
+end
+
 -- Makefile
-vim.keymap.set("n", "<leader>mb", ":!make<CR>",       { desc = "Make build" })
-vim.keymap.set("n", "<leader>mr", ":!make run<CR>",   { desc = "Make run" })
-vim.keymap.set("n", "<leader>mc", ":!make clean<CR>", { desc = "Make clean" })
+vim.keymap.set("n", "<leader>mb", function() run("make") end,       { desc = "Make build" })
+vim.keymap.set("n", "<leader>mr", function() run("make run") end,   { desc = "Make run" })
+vim.keymap.set("n", "<leader>mc", function() run("make clean") end, { desc = "Make clean" })
 
 -- CMake (preset psp)
-vim.keymap.set("n", "<leader>cb", ":!cmake --build --preset psp -j<CR>",                              { desc = "CMake build" })
-vim.keymap.set("n", "<leader>cc", ":!cmake --build --preset psp --target clean && echo 'Clean done!'<CR>",  { desc = "CMake clean" })
-vim.keymap.set("n", "<leader>ca", ":!cmake --preset psp && cmake --build --preset psp -j<CR>",        { desc = "CMake configure + build" })
-vim.keymap.set("n", "<leader>ci", ":!cmake --install build<CR>",                                      { desc = "CMake install" })
+vim.keymap.set("n", "<leader>cb", function() run("cmake --build --preset psp -j") end,                             { desc = "CMake build" })
+vim.keymap.set("n", "<leader>cc", function() run("cmake --build --preset psp --target clean && echo 'Clean done!'") end, { desc = "CMake clean" })
+vim.keymap.set("n", "<leader>ca", function() run("cmake --preset psp && cmake --build --preset psp -j") end,       { desc = "CMake configure + build" })
+vim.keymap.set("n", "<leader>ci", function() run("cmake --install build") end,                                     { desc = "CMake install" })
