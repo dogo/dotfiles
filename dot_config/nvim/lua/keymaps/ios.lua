dea85036376ae4b16c1945eee84fae8b8aa2bcf5
Diogo Autilio <diautilio@gmail.com>
2026-03-07 14:32:55 -0300

    feat: add key mapping for building and debugging iOS apps in Neovim

--- dot_config/nvim/lua/keymaps/ios.lua
+++ dot_config/nvim/lua/keymaps/ios.lua
@@ -19,6 +19,7 @@ vim.keymap.set("n", "<leader>xf", "<cmd>XcodebuildProjectManager<cr>", { desc = 
 vim.keymap.set("n", "<leader>xb", "<cmd>XcodebuildBuild<cr>", { desc = "Build Project" })
 vim.keymap.set("n", "<leader>xB", "<cmd>XcodebuildBuildForTesting<cr>", { desc = "Build For Testing" })
 vim.keymap.set("n", "<leader>xr", "<cmd>XcodebuildBuildRun<cr>", { desc = "Build & Run Project" })
+vim.keymap.set("n", "<leader>xD", function() require("xcodebuild.integrations.dap").build_and_debug() end, { desc = "Build & Debug App" })
 
 vim.keymap.set("n", "<leader>xt", "<cmd>XcodebuildTest<cr>", { desc = "Run Tests" })
 vim.keymap.set("v", "<leader>xt", "<cmd>XcodebuildTestSelected<cr>", { desc = "Run Selected Tests" })
--- dot_config/nvim/lua/plugins/ios/dap.lua
+++ dot_config/nvim/lua/plugins/ios/dap.lua
@@ -2,33 +2,15 @@ return {
   {
     "mfussenegger/nvim-dap",
     config = function()
+      require("xcodebuild.integrations.dap").setup()
+
       local dap = require("dap")
-
-      -- Configure lldb-dap adapter
-      dap.adapters["lldb-dap"] = {
-        type = "executable",
-        command = "xcrun",
-        args = { "lldb-dap" },
-        name = "lldb-dap",
-      }
-
-      -- Configure swift
-      dap.configurations.swift = {
-        {
-          name = "iOS App",
-          type = "lldb-dap",
-          request = "attach",
-          waitFor = true,
-          cwd = "${workspaceFolder}",
-        },
-      }
-
       vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint)
       vim.keymap.set("n", "<leader>dc", dap.continue)
       vim.keymap.set("n", "<leader>di", dap.step_into)
       vim.keymap.set("n", "<leader>do", dap.step_over)
       vim.keymap.set("n", "<leader>dO", dap.step_out)
-      vim.keymap.set("n", "<leader>dq", dap.terminate)
+      vim.keymap.set("n", "<leader>dq", require("xcodebuild.integrations.dap").terminate_session)
     end,
   },
 }
